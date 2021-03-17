9d0810a71a955105a30a62ff19c1747eafaf3c23
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-03-17 16:43:34 -0400

    Update database config

--- config/database.js
+++ config/database.js
@@ -4,16 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'portfolio-cluster.2lpdn.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'brenden-moore-strapi'),
-        username: env('DATABASE_USERNAME', 'brendenm17'),
-        password: env('DATABASE_PASSWORD', 'BAMf8rejk3'),
+        uri: env('DATABASE_URI')
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: true;
       },
     },
   },
